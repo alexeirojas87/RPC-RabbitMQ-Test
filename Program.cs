259b106d73084737638a6ff5d668bdadbcc06e09
alexeirojas87 <alexeirojas87@gmail.com>
2021-10-07 18:43:20 -0400

    Tested all type of methods and type of constructor

--- Program.cs
+++ Program.cs
@@ -19,13 +19,21 @@ namespace RabbitMQ_Test
         {
             var rpcClient = new ClientRPCRabbitMQWrapper("localhost");
 
-            Console.WriteLine(" [x] Requesting");
+            Console.WriteLine(" [x] Requesting TestMethod");
             var response = await rpcClient.CallAsync<string>(new RPCRequest() { Class = "RabbitMQ_Server.Test", Method = "TestMethod" });
             Console.WriteLine(" [.] Got '{0}'", response);
 
-            Console.WriteLine(" [x] Requesting");
+            Console.WriteLine(" [x] Requesting TestMethodObject");
             var responseObject = await rpcClient.CallAsync<RPCObjectReturn>(new RPCRequest() { Class = "RabbitMQ_Server.Test", Method = "TestMethodObject" });
             Console.WriteLine(" [.] Got '{0}'", (responseObject as RPCObjectReturn).MyProperty);
+
+            Console.WriteLine(" [x] Requesting TestMethodObjectwithParam");
+            var responseObjectWP = await rpcClient.CallAsync<RPCObjectReturn>(new RPCRequest() { Class = "RabbitMQ_Server.Test", Method = "TestMethodObjectwithParam", ParamsMethod = new object[] { 2 } });
+            Console.WriteLine(" [.] Got '{0}'", (responseObjectWP as RPCObjectReturn).MyProperty);
+
+            Console.WriteLine(" [x] Requesting TestMethodObjectwithParamAndConstructorParam");
+            var responseObjectCWP = await rpcClient.CallAsync<RPCObjectReturn>(new RPCRequest() { Class = "RabbitMQ_Server.Test", Method = "TestMethodObjectwithParam", ParamsMethod = new object[] { 3 }, ParamsConstructor = new object[] { 3 } });
+            Console.WriteLine(" [.] Got '{0}'", (responseObjectCWP as RPCObjectReturn).MyProperty);
 
             rpcClient.Close();
         }
